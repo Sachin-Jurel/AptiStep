706855b2849dfeb24144aa0731b3c7c6e083f568
Sachin-Jurel <sachinjurel82@gmail.com>
2025-08-31 11:29:43 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors);
-app.use(express.json);
+app.use(cors({
+     origin: "http://localhost:5173",   
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true   
+}));
+app.use(express.json());
 
 app.get('/', (req, res)=>{
     console.log("home page");
+})
+
+const val = {
+    name: "sachin",
+    age: 20,
+}
+
+app.get('/data', (req, res)=>{
+    return res.json({val});
 })
 
 app.listen("8080", ()=>{
